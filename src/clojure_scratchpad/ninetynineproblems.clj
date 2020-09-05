c6c1e158360f1202e38929c809c7ed7fa4898b5b
Malik Kennedy <mksybr@gmail.com>
2020-09-04 17:00:55 -0700

    Moved file, copied a prolog problem from the web.

--- src/clojure_scratchpad/ninetynineproblems.clj
+++ src/clojure_scratchpad/ninetynineproblems.clj
@@ -1,5 +1,7 @@
-(ns ninetynineproblems)
+(ns clojure-scratchpad.ninetynineproblems
+  (:require [clojure.core.logic]))
 
+;;;;; 99 Lisp Problems
 (defn my-last [args]
   (if (= 1 (count args))
     (first args)
@@ -138,4 +140,49 @@
   (for [[a b & rst] lst]
     (if (= a b) a
         [a b])))
+
+
+
+
+;;; 99 Prolog Problems
+(use 'clojure.core.logic)
+
+(run 2 (q)
+  (fresh [a b c d]
+    (== d  [a b c q])))
+
+; Find the last element of a list
+(defn lasto [last list]
+  (matche [list]
+    ([[]]          fail)
+    ([[last]]      succeed)
+    ([[_ . ?rest]] (lasto last ?rest))))
+
+
+(run* (q)
+  )
+
+
+(run 3 [q r]
+  (fresh [a b]
+    (== (lasto q a) b)
+    (== r b)
+    (== q r)))
+
+(run* [q]
+  (== q {:a 2 :b 2}))
+
+(run 3 [q]
+  (membero q [1 2 3]))
+
+(run 3 [q]
+  (membero q [q 2 3]))
+;; => (_0 2 3)
+
+
+;; I've heard of a SET INTERSECTION before. 
+(run* [q]
+  (membero q [1 2 3])
+  (membero q [3 4 5]))
+
 
