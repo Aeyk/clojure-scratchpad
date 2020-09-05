9976352928629f5ef4af2884fd8262d682dfb212
Malik Kennedy <mksybr@gmail.com>
2020-09-04 21:46:08 -0700

    Map associative destructuring

--- src/clojure_scratchpad/clojure-workshop.clj
+++ src/clojure_scratchpad/clojure-workshop.clj
@@ -562,3 +562,43 @@
     {:from {:lat 37.742 :lon -25.6976 :name "Ponta Delgada Airport"},
      :to {:lat 48.9615 :lon 2.4372 :name "Paris Le Bourget Airport"}}]})
 
+
+(def mapjet-booking
+  {:id 8773
+   :customer-name "Alice Smith"
+   :catering-notes "Vegetarian on Sundays"
+   :flights
+   [{:from {:lat 48.9615 :lon 2.4372 :name "Paris Le Bourget Airport"},
+     :to {:lat 37.742 :lon -25.6976 :name "Ponta Delgada Airport"}},
+    {:from {:lat 37.742 :lon -25.6976 :name "Ponta Delgada Airport"},
+     :to {:lat 48.9615 :lon 2.4372 :name "Paris Le Bourget Airport"}}]})
+(let [{:keys [customer-name flights]} mapjet-booking]
+  (println (str customer-name " booked " (count flights) " flights.")))
+
+
+(defn print-mapjet-flight [flight]
+  (for [flight flights]
+    (let [{:keys [to from]} flight
+          {:keys [lat lon]} to
+          lat-1 lat lon-1 lon]
+      (let [{:keys [lat-2 lon-2]} from
+            lat-2 lat lon-2 lon]
+        (println (str "Flying from: Lat " lat-1 " Lon " lon-1 " Flying to: Lat " lat-2 " Lon " lon-2))))))
+mapjet-booking
+(defn print-mapjet-flight [flight]
+  (let [{:keys [customer-name flights]} mapjet-booking] 
+    (for [flight flights]
+      (let [{:keys [from to]} flight
+            {lat1 :lat lon1 :lon} from
+            {lat2 :lat lon2 :lon} to]
+        (println (str "Flying from: Lat " lat1 " Lon " lon1 " Flying to: Lat " lat2 " Lon " lon2))))))
+
+;; Notice that we cannot use the shorter syntax for extracting coordinates 
+;;	 because the names lat and lon would conflict; therefore, we used
+;;	 the normal syntax, allowing us to explicitly declare a new binding
+;;	 to symbols with different names.As with vectors, we can nest destructuring
+;;	 expressions, and even combine the two techniques.
+
+
+
+(print-mapjet-flight mapjet-booking)
