d74ab838fadde0ca03561bceec16eaae4888b181
Malik Kennedy <mksybr@gmail.com>
2020-09-04 21:00:41 -0700

    Few more examples pilfered from core.logic's test cases

--- src/minikanren/byrd.clj
+++ src/minikanren/byrd.clj
@@ -3,6 +3,42 @@
   (:use [clojure.core.logic]
         [clojure.core.logic.pldb]
         [clojure.tools.macro :as macro]))
+
+
+(defn pairo [p]
+  (fresh [a d]
+    (== (lcons a d) p)))
+
+(defn twino [p]
+  (fresh [x]
+    (conso x x p)))
+
+(defn listo [l]
+  (conde
+    [(emptyo l) s#]
+    [(pairo l)
+     (fresh [d]
+       (resto l d)
+       (listo d))]))
+
+(defn flatteno [s out]
+  (conde
+    [(emptyo s) (== '() out)]
+    [(pairo s)
+     (fresh [a d res-a res-d]
+       (conso a d s)
+       (flatteno a res-a)
+       (flatteno d res-d)
+       (appendo res-a res-d out))]
+    [(conso s '() out)]))
+
+(defn anyo [q]
+  (conde
+    [q s#]
+    [(anyo q)]))
+
+
+
 (run 1 [q]
   (fresh [x y z]
     (== x z)
@@ -33,4 +69,29 @@
       ((== fail q))
       ((== succeed q))
       ((loop)))))
+
+(run 6 [q]
+  (conde
+    ((anyo (== false q)))
+    ((== true q))))
+
+
+;; (run* [q]
+;;   (anyo
+;;     (conde
+;;       ((== 1 q))
+;;       ((== 2 q))
+;;       ((== 3 q))
+;;       )))
+
+
+(def alwayso
+  (anyo (== false false)))
+
+(run 1
+  (x)
+  (== true x)
+  alwayso
+  (== true x))
+
 
