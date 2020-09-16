227499027521db267fd3943ebf9c787c219f559a
Malik Kennedy <mksybr@gmail.com>
2020-09-16 14:22:51 -0700

    Modified pascals triangles to work for pascals rectangles

--- src/exercises.clj
+++ src/exercises.clj
@@ -9,24 +9,32 @@
 (next-row 2)
 ;; stolen from rosetta code
 (fn [n]
-    (let [pascal 
-          (iterate #(concat [1] 
-                      (map + % (rest %)) 
-                      [1]) 
-            [1])]
-      (last (take n pascal))))
-1
+  (let [pascal 
+        (iterate
+          #(concat
+             [1] 
+             (map + % (rest %))
+             [1])
+          [1])]
+    (last (take n pascal))))
+
+
+(take 2
+  (fn [x]
+      (let [pascal 
+            (iterate
+              #(concat
+                 [(first %)]
+                 (map +' % (rest %))
+                 [(last %)])
+              x)]
+        pascal))
+  [2 3 2])
+
+
+2 [2 3 2]
 
 (take 10 pascal)
- 
-
-
-;;Given a side-effect free function f and an initial value x write a function which returns an infinite lazy sequence of x, (f x), (f (f x)), (f (f (f x))), etc.
-
-(fn my-iterate
-  [f x]
-  (lazy-seq
-    (cons x (my-iterate f (f x)))))
 
 
 (defn positive-numbers 
@@ -34,4 +42,6 @@
   (lazy-seq
     (cons n (positive-numbers (inc n)))))
 
-(take 3 (my-iterate inc 1))
+#_(take 3 (my-iterate inc 1))
+
+
