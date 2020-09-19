cbdccff6eafbda5b0a61439c104082e01437e944
Malik Kennedy <mksybr@gmail.com>
2020-09-18 23:10:24 -0700

    Coding exercises FOR DAAYYS

--- src/cracking_the_coding_interview/arrays_and_strings.clj
+++ src/cracking_the_coding_interview/arrays_and_strings.clj
@@ -1,4 +1,5 @@
 (ns cracking-the-coding-interview.arrays-and-strings)
+
 ;;;  Implement an algorithm to determine
 ;;; 	if a string has all unique
 ;;;	characters.
@@ -27,11 +28,6 @@
         (recur (rest chs)
           (conj known (first chs)))))))
 
-
-  
-
-
-
 ;; What if you could not use any
 ;;	preexisting datastructures
 
@@ -57,3 +53,141 @@
         cnil)))
 
 (reverse-c-string "Hello World\n\0")
+
+
+;;;Design  an  algorithm  and  write  code  to  remove  the  duplicate  characters  in  a  string without using any additional buffer  NOTE: One or two additional variables are fineAn extra copy of the array is not
+(defn my-dedupe
+  [coll]
+  (loop [coll  coll
+         known []
+         pk   (first coll)]
+    (if (empty? coll)
+      known
+      (if (= pk (first coll))
+        (recur
+          (rest coll)
+          known
+          pk)
+        (recur
+          (rest coll)
+          (conj known pk)
+          (first coll))))))
+;; Wow!! I liked this, when I solved it wasn't sure if I solved it.
+(my-dedupe (flatten
+             (into [] (zipmap (range 10) (range 10)))))
+;; => [0 7 1 4 6 3 2 9 5]
+(flatten
+  (into [] (zipmap (range 10) (range 10))))
+;; => (0 0 7 7 1 1 4 4 6 6 3 3 2 2 9 9 5 5 8 8)
+
+;;; TODO: ;Write the test cases for this method
+
+
+;;; Write a method to decide if two strings are anagrams or not
+
+;;; Count char occurences
+;;; Compare
+
+#_(frequencies) "Hello world!"
+
+
+(defn anagrams?
+  [s1 s2]
+  (let [s1 (clojure.string/replace s1 #"\s" "")
+        s2 (clojure.string/replace s2 #"\s" "")]
+    (=
+      (frequencies s1)
+      (frequencies s2))))
+
+(anagrams?
+  "red rum"
+  "murder")
+;; => true
+
+;;; <2020-09-18 Fri 22:29>
+;;; Write a method to replace all spaces in a string with ‘%20’
+(defn space-encoding [s]
+  (clojure.string/replace s " " "%20"))
+(space-encoding "Hello world!")
+;; => "Hello%20world!"
+
+;;;; <2020-09-18 Fri 22:30>
+
+;;; Given  an  image  represented  by  an  NxN
+;;; 	matrix,  where  each  pixel  in  the  image
+;;;	is  4 bytes, write a method to rotate the image
+;;;	by 90 degrees  Can you do this in place?
+
+(defn znxm [n m]
+  (for [x (repeat n 0)]    
+    (repeat m x)))
+
+#_(defn znxm [n m]
+  (for [x (range 1 (inc n))]
+    (cycle x)
+    #_(repeat m x)))
+
+(defn nxm [n m]
+  (partition n
+    (apply interleave
+      (for [x (range n)]
+        (repeat m x)))))
+
+
+
+(let [nxma (nxm 3 3)]
+
+  (for [x (range (count nxma))]
+    (map #(nth (drop 0 nxma) x) nxma))
+  )
+
+#_(for [x nxma]
+  (map #(nth nxma %) x))
+
+(for [x (count nxm)]
+  (map #(nth % x))
+  (vec (map vec nxm)))
+  
+;; => ((0 1 2) (0 1 2) (0 1 2))
+
+
+
+(defn coordsnxm [n m]
+  (for [x (range n)
+        y (range m)]
+    [x y]))
+
+(coordsnxm 2 2)
+;; => ([0 0] [0 1] [1 0] [1 1])
+
+(map first (nxm 4 3))
+
+(for [row (nxm 4 3)
+      :let [nxm (nxm 4 3)]]
+  (map #(nth % 0) nxm))
+
+
+(map #(nth % 0) (nxm 4 3))
+
+
+(defn rotate-clockwise
+  [m]
+  (for [row (range (inc (count m)))]
+    (map #(nth % row) m)))
+
+;; => (nxm 4 3)
+((0 1 2 3)
+ (0 1 2 3)
+ (0 1 2 3))
+;; (rotate-clockwise (nxm 4 3))
+((0 0 0)
+ (1 1 1)
+ (2 2 2)
+ (3 3 3))
+
+
+;;; That was fun... Hard but fun. <2020-09-18 Fri 23:10>
+
+
+
+
--- src/exercises.clj
+++ src/exercises.clj
@@ -1,5 +1,5 @@
 (require '[clojure.core.match :refer [match]])
-
+(require '[clojure.math.numeric-tower :as math])
 
 (defn next-row [row]
   (map +
@@ -87,3 +87,112 @@
 
 (phone-number "613.995.0253")
 ;; => "6139950253"
+ 
+
+(def old {1 ["A" "E" "I" "O" "U" "L" "N" "R" "S" "T"]
+          2 ["D" "G"]
+          3 ["B" "C" "M" "P"]
+          4 ["F" "H" "V" "W" "Y"]
+          5 ["K"]
+          8 ["J" "X"]
+          10 ["Q" "Z"]})
+
+(defn make-score-index-to-key-pair  [] 
+    (map (fn [[k vs]] 
+         (for [v vs]
+           [v k]))
+      old))
+
+
+(fn symmetric-difference
+  [a b]
+  (let [longer
+        (first
+          (sort-by #(count %1) [a b]))
+        shorter
+        (second
+          (sort-by #(count %1) [a b]))
+        isection (filter #(contains? shorter %) longer)]
+    
+    ;; what happens when you sort a collection without an idea of order-ness like a set?
+    (clojure.set/difference
+      (into a b)
+      isection)))
+
+
+
+(defn binary-tree?
+  [btree]
+  (every? true?
+      (let [c (for [binary-pair
+                    (tree-seq next ()rest btree)]
+                (if (seq? binary-pair)
+                  (or (= 3 (count binary-pair))
+                    (= 0 (count binary-pair)))
+                  true))]
+        c)))
+
+(defn binary-tree?
+  [btree]
+  (every? true?
+      (let [c (for [binary-pair
+                    (tree-seq next rest btree)]
+                (or (= 3 (count binary-pair))
+                  (= 0 (count binary-pair))))]
+        c)))
+;; took me almost an hour at the repl :(
+;; still doesnt work with the false test case, even though it's good
+(binary-tree? [1 [2 [3 [4 false nil] nil] nil] nil])
+
+
+
+(for [binary-pair (tree-seq next rest [1 [2 [3 [4 nil nil] nil] nil] nil])]
+  (count binary-pair))
+(binary-tree? [1 [2 [3 [4 nil nil] nil] nil ] nil])
+((comp count second) [1 [2 [3 [4 nil nil nil] nil] nil] nil])
+;; => 3
+
+(first [1 [2 [3 [4 nil nil nil] nil] nil] nil])
+;; => 1
+(second [1 [2 [3 [4 nil nil nil] nil] nil] nil])
+;; => [2 [3 [4 nil nil nil] nil] nil]
+(last [1 [2 [3 [4 nil nil nil] nil] nil] nil])
+;; => nil
+
+
+
+(fn gcd [x y]
+  (cond
+    (> x y)
+    (gcd (- x y) y)
+    (< x y)
+    (gcd x (- y x))
+    :else x))
+
+(fn lcm
+  ([a b]
+   (let [gcd
+         (fn gcd [x y]
+           (cond
+             (> x y)
+             (gcd (- x y) y)
+             (< x y)
+             (gcd x (- y x))
+             :else x))]
+     (/ 
+       (math/abs (* a b))
+       (gcd a b))))
+  ([a b & cs]
+   (reduce lcm (into [a b] cs))))
+
+
+(reduce lcm [(/ 3 4) (/ 1 6)])
+(Math/abs (/ 1 6))
+
+(dedupe [1 1 2 1 1 1 3 3])
+(vals (group-by identity [:a :a :b :b :c]))
+
+(defn pack [s]
+  (vals (group-by identity s)))
+
+(dedupe [1 1 2 1 1 1 3 3])
