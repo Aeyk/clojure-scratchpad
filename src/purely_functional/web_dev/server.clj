4f3b3b90b293f37ad2b8ae50ab6439d1f9625c3a
Malik Kennedy <mksybr@gmail.com>
2020-09-21 16:29:09 -0700

    Request Handler that shows you a edn dump of your Ring Request[1]
    1: https://ring-clojure.github.io/ring/ring.util.request.html

--- src/purely_functional/web_dev/server.clj
+++ src/purely_functional/web_dev/server.clj
@@ -12,9 +12,14 @@
   [req]
   {:status 200 :body "Good Bye Cruel World!" :headers {}})
 
+(defn request-handler [req]
+  {:status 200 :body (str req) :headers {}})
+
+
 (defroutes app
   (GET "/" [] greet)
   (GET "/goodbye" [] goodbye)
+  (GET "/request" [] request-handler)
   (not-found "Page Not Found."))
 
 (defn -main [port]
