272005bd9286eee8c81a52a7e47901dc4592eb1c
Malik Kennedy <mksybr@gmail.com>
2020-09-24 13:09:04 -0700

    Control flow and #t #f Brave and True

--- src/brave_and_true.clj
+++ src/brave_and_true.clj
@@ -7,3 +7,33 @@
 
 [0 "a item" #{}] ;; => [0 "a item" #{}]
 
+;;; Control Flow
+(if true
+  1 2);; => 1
+(if (or nil false)
+  1 2);; => 2
+(if (or nil false)
+  1);; => nil
+
+
+(if true
+  (do
+    (println "Hello and thank you")
+    1)
+  (do
+    (println "Get out and good riddance!")
+    2)) 
+;;"Hello and thank you"
+;; => 1
+
+;;; Use when if you want to do multiple things when some condition is
+;;;	true, and you always want to return nil when the condition is false.
+(when true
+  (println "Where are you going?")
+  "I'm going to this point.")
+;; => "I'm going to this point."
+
+(def falsey? (or nil? false?))
+(def truthy? (complement falsey?))
+
+
