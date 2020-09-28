856040cddab780c0b4d3afdf7d86ab021066d929
Malik Kennedy <mksybr@gmail.com>
2020-09-28 16:03:39 -0700

    Exercism Robot Name Covered all tests

--- src/exercism/robot_name.clj
+++ src/exercism/robot_name.clj
@@ -1,17 +1,18 @@
 (ns robot-name)
 
+(def *robot* ^:dynamic (atom ""))
+
 (defn robot []
   (let [alphabet (map char (range 65 91))
         r1 (first (shuffle alphabet))
         r2 (first (shuffle alphabet))
         [d1 d2 d3] (shuffle (range 10))]
-    (str r1 r2 d1 d2 d3)))
+    (swap! *robot* (fn [x]
+                     (str r1 r2 d1 d2 d3)))))
     
               
 (defn robot-name [r]
+  @*robot*)
+
+(defn reset-name [r]
   (robot))
-
-
-(defn reset-name [robot] ;; <- arglist goes here
-  ;; your code goes here
-  )
