c0d4591470cfe0bf31b6952228f5f7f0a638dde1
Malik Kennedy <mksybr@gmail.com>
2020-09-28 14:18:28 -0700

    Stole a solution to nucleotide count.
    Mostly because I didnt understand how they wnated the division between
    2 fns.

--- src/exercism/nucleotide_count.clj
+++ src/exercism/nucleotide_count.clj
@@ -1,8 +1,11 @@
-(defn nucleotide-count
-  [nucs]
-  (into {}
-    (for [grp
-          (partition-by identity nucs)]
-      [(keyword (str (first grp)))
-       (count grp)])))
+(ns nucleotide-count)
  
+;;; https://exercism.io/tracks/clojure/exercises/nucleotide-count/solutions/bc1e2294f9a84d50ad7a3fa73f542648
+
+(defn nucleotide-counts [s]
+  (merge {\A 0, \C 0, \G 0, \T 0} (frequencies s)))
+
+(defn count-of-nucleotide-in-strand 
+  [n strand]
+  {:pre [(#{\G \C \T \A} n)]}
+  ((nucleotide-counts strand) n))
