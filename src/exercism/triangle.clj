a7ae4afecec6f9959bef9866830cb9e556185d50
Malik Kennedy <mksybr@gmail.com>
2020-09-28 20:58:55 -0700

    Exercism Triangle DONE

--- src/exercism/triangle.clj
+++ src/exercism/triangle.clj
@@ -1,19 +1,31 @@
 (ns triangle)
 
 (defn is-valid? [x y z]
-  (and (< 0 (+ x y z))
-    (or (>= (+ x y) z)
-      (>= (+ y z) x))))
+  (and (every? #(< 0 %) [x y z])
+    (and
+      (>= (+ y z) x)
+      (>= (+ z x) y)
+      (>= (+ x y) z))))
+
+(defn scalene? [x y z]
+  (and
+    (is-valid? x y z)
+    (not= x y)
+    (not= y z)
+    (not= z x)
+    (not= x y z)
+    (not= 0 x y z)))
 
 
 (defn equilateral? [x y z]
-  )
+  (and
+    (not= 0 x y z)
+    (= x y z)))
 
 (defn isosceles? [x y z]
-  (and (is-valid? x y z)
+  (and
+    (not= 0 x y z)
+    (is-valid? x y z)
     (or (= x y)
-      (= y z))))
-
-(defn scalene? [x y z]
-  (and (is-valid? x y z)
-    (not= x y z)))
+      (= y z)
+      (= z x))))
