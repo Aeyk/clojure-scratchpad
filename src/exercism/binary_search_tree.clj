81635b9e633caf1530cb1cb8441895a3e91a79d9
Malik Kennedy <mksybr@gmail.com>
2020-10-04 19:42:48 -0700

    Exercism BST: Insertion on empty coll calls singleton

--- src/exercism/binary_search_tree.clj
+++ src/exercism/binary_search_tree.clj
@@ -3,10 +3,19 @@
 (defn singleton [v] {:value v
                      :left nil
                      :right nil})
+
 (defn value [e] (get e :value))
 (defn right [t] (get t :right))
 (defn left [t] (get t :left))
 
-(defn insert [e tree])
+(defn insert [tree e]
+  (if (empty? tree)
+    (singleton e)))
+
 (defn from-list [lst] lst)
 (defn to-list [tree] tree)
+
+(singleton 4)
+;; => {:value 4, :left nil, :right nil}
+
+(insert [] 4)
