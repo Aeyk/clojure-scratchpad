685b3723da3a784cdbc1e971101e0925520ccabc
Malik Kennedy <mksybr@gmail.com>
2020-11-30 22:35:07 -0800

    Remove copied code

--- src/aoc/one.clj
+++ src/aoc/one.clj
@@ -4,14 +4,6 @@
 
 (def data (clojure.string/split (slurp "./resources/1.1") #"\n"))
 
-(defn sumo [vars sum]
-  (fresh [vhead vtail run-sum]
-    (conde
-     [(== vars ()) (== sum 0)]
-     [(conso vhead vtail vars)
-      (fd/+ vhead run-sum sum)
-      (sumo vtail run-sum)])))
-
 (run 1 [q]
   (fresh [a b]
     (fd/in a b (fd/interval 0 2020))
