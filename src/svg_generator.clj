f8edddd91cbd3f2ac1dacf56baae617cc6bf40e7
Malik Kennedy <mksybr@gmail.com>
2020-12-19 16:15:40 -0800

    feat: connect every point to each other

--- src/svg_generator.clj
+++ src/svg_generator.clj
@@ -1,13 +1,13 @@
 (ns svg-generator
   (:require [clojure.math.numeric-tower :as math]
+            [clojure.math.combinatorics :as combo]
             [analemma.svg :as svg]
             [analemma.charts :as charts]
-            [hiccup.core :as html])
-  (:import java.lang.Math))
+            [hiccup.core :as html]
+            [net.cgrand.enlive-html :as selector]
+            [clojure.set :as set]
+            [clojure.edn :as edn]))
 
-(spit
- "test-1.svg"
- #_(emit-svg))
 (let [x (range -1 1 0.005)
       y1 (map #(math/expt 1000 %) x)
       y2 (map #(math/expt 10 %) x)
@@ -23,20 +23,9 @@
       #_(charts/add-points [x y1] :transpose-data?? true)
       #_(charts/add-points [x y2] :transpose-data?? true
                            :color (svg/rgb 255 0 0))))
-
-;;; https://stackoverflow.com/questions/14633363/can-i-apply-a-gradient-along-an-svg-path
-(spit "hello.svg"
-      (html/html
-       [:svg#artistry-aint-dead
-        {:viewBox "-400 -500 800 800" :xmlns "http://www.w3.org/2000/svg"}
-        [:style "circle { stroke: url(#gradient); }"]
-        [:linearGradient#gradient {:x1 "0%" :y1 "0%" :x2 "100%" :y2 "100%"}
-         [:stop {:offset "0%" :stop-color "#fff"}]        
-         [:stop {:offset "25%" :stop-color "#32f"}]
-         [:stop {:offset "50%" :stop-color "#03d"}]
-         [:stop {:offset "100%" :stop-color "#e1e1"}]]
-        #_(draw-n-circles-m-px-from-point 10 01)
-        (map draw-circle-nth-around-circle (range 0 (inc satellite-count) 1))]))
+(defn draw-circle [x y r]
+  [:circle {:cx x :cy y  :r r
+            :fill "transparent"}])
 
 (defn polar->cartesian [cx cy r angle-in-degrees]
   (let [angle-in-radians (* angle-in-degrees (/ java.lang.Math/PI 180.0))]
@@ -45,33 +34,123 @@
      :y (* (java.lang.Math/sin angle-in-radians)
            (+ cy  r))}))
 
-(def satellite-count 99)
+(def satellite-count  3)
 (def c 200)
 
-(defn draw-circle [x y r]
-  [:circle {:cx x :cy y  :r r
-            :stroke "white"
-            :fill "transparent"}])
+(defn draw-n-circles-grouped-around-circle-of-r-radius [n r group-id]
+  [(keyword (str "g#" group-id))
+   (map draw-nth-circle-around-circle-of-r-radius
+        (range 0 n)
+        (repeat r)
+        (repeat n))])
 
 ;;; https://stackoverflow.com/questions/28992878/svg-a-circle-of-circles
-(defn draw-circle-nth-around-circle [n]
-  (let [º-of-seperation (/ 360 satellite-count)
-        coords (polar->cartesian c c 100 (* n º-of-seperation))]
-    (draw-circle (:x coords) (:y coords) 3)))
+(defn draw-nth-circle-around-circle-of-r-radius [n r c]
+  (let [satellite-count c
+        º-of-seperation (/ 360 satellite-count)
+        coords (polar->cartesian c c r (* n º-of-seperation))]
+    (draw-circle (:x coords) (:y coords) 10)))
 
-(defn draw-n-circles-m-px-from-point [n m]
-  (let [angles (range 0 n)
-        step (/ (* Math/PI 2)
-                m)
-        xs (map #(* c (Math/sin %) angles))
-        ys (map #(* c (Math/cos %) angles))]
-    (map (fn [x y]
-           (draw-circle x y))
-         xs ys)))
+(defn draw-n-circles-around-circle-of-r-radius [n r]
+  (map draw-nth-circle-around-circle-of-r-radius
+       (range 0 n)
+       (repeat r)
+       (repeat n)))
 
-;; for(var n=0; n<number_of_satellite_circles; n++){
-;;      var degrees_of_separation = 360/number_of_satellite_circles
-;;      var angle_as_degrees = degrees_of_separation * n
-;;      var coordinates = polarToCartesian(cx, cy, radius_of_satellites_from_center, angle_as_degrees)
-;;      document.getElementById('my_svg').appendChild( drawCircle(coordinates.x,coordinates.y,radius_of_small_circles) )
+;;; https://stackoverflow.com/questions/14633363/can-i-apply-a-gradient-along-an-svg-path
+(spit "hello.svg"
+      (html/html
+       [:svg#artistry-aint-dead
+        {:viewBox "-400 -400 800 800" :xmlns "http://www.w3.org/2000/svg"}
+        [:style "g#a > circle { fill: red; }
+g#b > circle { fill: blue; }
+g#c > circle { fill: green; }
+"]
+        [:linearGradient#gradient {:x1 "0%" :y1 "0%" :x2 "100%" :y2 "100%"}
+         [:stop {:offset "0%" :stop-color "#fff"}]
+         [:stop {:offset "25%" :stop-color "#32f"}]
+         [:stop {:offset "50%" :stop-color "#03d"}]
+         [:stop {:offset "100%" :stop-color "#e1e1"}]]
+        (draw-n-circles-grouped-around-circle-of-r-radius 3 100 "a")
+        (draw-n-circles-grouped-around-circle-of-r-radius 4 100 "b")
+        (draw-n-circles-grouped-around-circle-of-r-radius 5 100 "c")]))
+
+(defn lines-to-each-circle []
+  (map
+   line
+   (partition
+    2
+    (partition
+     2
+     (flatten 
+      (filter (complement nil?)
+              (map (comp #(Math/round %) first :cx) (get-circle-coordinates svg))))
+     #_(filter (complement nil?)
+               (map (comp first :cx) (get-circle-coordinates svg)))))))
+
+
+(def svg (html/html
+          [:svg#artistry-aint-dead
+           {:viewBox "-400 -400 800 800" :xmlns "http://www.w3.org/2000/svg"}
+           [:style "g#a > circle { fill: red; }
+g#b > circle { fill: blue; }
+g#c > circle { fill: green; }
+"]
+           [:linearGradient#gradient {:x1 "0%" :y1 "0%" :x2 "100%" :y2 "100%"}
+            [:stop {:offset "0%" :stop-color "#fff"}]
+            [:stop {:offset "25%" :stop-color "#32f"}]
+            [:stop {:offset "50%" :stop-color "#03d"}]
+            [:stop {:offset "100%" :stop-color "#e1e1"}]]
+           (draw-n-circles-grouped-around-circle-of-r-radius 3 100 "a")
+           (draw-n-circles-grouped-around-circle-of-r-radius 4 100 "b")
+           (draw-n-circles-grouped-around-circle-of-r-radius 5 100 "c")
+           [:g 
+            (draw-lines)]]))
+
+(spit "hello.svg" svg)
+
+
+(defn get-coordinates [doc elem]
+  (mapcat
+   #(vector {:cx (selector/attr-values % :cx)}
+            {:cy (selector/attr-values % :cy)})
+   (selector/select (selector/html-snippet doc) elem)))
+
+(defn draw-lines []
+  (map line
+       (partition
+        2
+        (map vals
+             (flatten
+              (combo/combinations
+               (map
+                (comp
+                 #(update % :y (comp (fn [e] (Math/round e)) edn/read-string))
+                 #(update % :x (comp (fn [e] (Math/round e)) edn/read-string))
+                 #(dissoc % :r)
+                 #(dissoc % :fill)
+                 #(set/rename-keys % {:cx :x :cy :y})
+                 #(assoc % :fill "black"))
+                (map :attrs (selector/select (selector/html-snippet svg) [:circle])))
+               2))))))
+
+(defn get-circle-coordinates [doc]
+  (get-coordinates doc [:circle]))
+
+#_(defn draw-lines []
+  (map line
+       (combo/permutations 
+        (for [[[a _] [_ b]]
+              (partition 2 (map (juxt :cx :cy) (get-circle-coordinates svg)))]
+          #_(zipmap
+             [:x :y])
+          (apply vector (reduce merge a b))))))
+
+
+(defn line [[[x1 y1]
+             [x2 y2]]]
+  [:line {:stroke "black"
+          :x1 x1 :y1 y1 :x2 x2 :y2 y2}])
+
+
 
