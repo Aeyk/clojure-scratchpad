3ab903a4f6559c9d73fa0aacda6fc6ec3ac432e0
Malik Kennedy <mksybr@gmail.com>
2021-01-05 14:26:21 -0800

    chore: Reitit Routing Exampple

--- src/clojure_scratchpad/frontend.cljs
+++ src/clojure_scratchpad/frontend.cljs
@@ -5,47 +5,66 @@
    [reitit.frontend :as rf]
    [reitit.frontend.easy :as rfe]
    [reitit.coercion.spec :as rss]
-   [clojure-scratchpad.frontend.counter :as counter]))
+   [spec-tools.data-spec :as ds]
+   [fipp.edn :as fedn]))
 
-(defn app
-  []
-  [:p "HELLO"
-   ])
+(defn home-page []
+  [:div
+   [:h2 "Welcome to frontend"]])
+
+(defn about-page []
+  [:div
+   [:h2 "About frontend"]
+   [:ul
+    [:li [:a {:href "http://google.com"} "external link"]]
+    [:li [:a {:href (rfe/href ::foobar)} "Missing route"]]
+    [:li [:a {:href (rfe/href ::item)} "Missing route params"]]]
+   [:div
+    {:content-editable true
+     :suppressContentEditableWarning true}
+    [:p "Link inside contentEditable element is ignored."]
+    [:a {:href (rfe/href ::frontpage)} "Link"]]])
+
+(defn item-page [match]
+  (let [{:keys [path query]} (:parameters match)
+        {:keys [id]} path]
+    [:div
+     [:h2 "Selected item " id]
+     (if (:foo query)
+       [:p "Optional foo query param: " (:foo query)])]))
+
+(defonce match (r/atom nil))
+
+(defn current-page []
+  [:div
+   [:ul
+    [:li [:a {:href (rfe/href ::frontpage)} "Frontpage"]]
+    [:li [:a {:href (rfe/href ::about)} "About"]]
+    [:li [:a {:href (rfe/href ::item {:id 1})} "Item 1"]]
+    [:li [:a {:href (rfe/href ::item {:id 2} {:foo "bar"})} "Item 2"]]]
+   (if @match
+     (let [view (:view (:data @match))]
+       [view @match]))])
 
 (def routes
   [["/"
     {:name ::frontpage
-     :view app}]
+     :view home-page}]
 
-   #_["/about"
-    {:name :about
-     :view button}]
+   ["/about"
+    {:name ::about
+     :view about-page}]
 
-   #_["/item/:id"
+   ["/item/:id"
     {:name ::item
      :view item-page
      :parameters {:path {:id int?}
-                  :query {(rss/opt :foo) keyword?}}}]])
-
-(def router
-  (rf/router routes {:data {:coercion rss/coercion}}))
-
-(defonce match (atom nil))
-
-(defn on-navigate-fn
-  [m]
-  (js/console.log m)
-  (reset! match m))
-
-(defn init-router
-  []
-  (rfe/start!
-   router
-   on-navigate-fn
-   {:use-fragment false}))
+                  :query {(ds/opt :foo) keyword?}}}]])
 
 (defn init []
-  (init-router)
-  (rd/render
-   (app)
-   (js/document.getElementById "app")))
+  (rfe/start!
+    (rf/router routes {:data {:coercion rss/coercion}})
+    (fn [m] (reset! match m))
+    ;; set to false to enable HistoryAPI
+    {:use-fragment true})
+  (rd/render [current-page] (.getElementById js/document "app")))
