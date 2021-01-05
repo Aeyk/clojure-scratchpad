09957edbc1594efc5429ffd7fd7ec72682512128
Malik Kennedy <mksybr@gmail.com>
2021-01-05 13:01:21 -0800

    chore: convert rum to reagent

--- src/clojure_scratchpad/frontend.cljs
+++ src/clojure_scratchpad/frontend.cljs
@@ -1,15 +1,51 @@
 (ns clojure-scratchpad.frontend
   (:require
-   [rum.core :as rum]
+   [reagent.core :as r]
+   [reagent.dom :as rd]
+   [reitit.frontend :as rf]
+   [reitit.frontend.easy :as rfe]
+   [reitit.coercion.spec :as rss]
    [clojure-scratchpad.frontend.counter :as counter]))
 
-(rum/defc app
+(defn app
   []
-  [(counter/sign-up-form)
-   (counter/post-comment-form)
+  [:p "HELLO"
    ])
 
+(def routes
+  [["/"
+    {:name ::frontpage
+     :view app}]
+
+   #_["/about"
+    {:name :about
+     :view button}]
+
+   #_["/item/:id"
+    {:name ::item
+     :view item-page
+     :parameters {:path {:id int?}
+                  :query {(rss/opt :foo) keyword?}}}]])
+
+(def router
+  (rf/router routes {:data {:coercion rss/coercion}}))
+
+(defonce match (atom nil))
+
+(defn on-navigate-fn
+  [m]
+  (js/console.log m)
+  (reset! match m))
+
+(defn init-router
+  []
+  (rfe/start!
+   router
+   on-navigate-fn
+   {:use-fragment false}))
+
 (defn init []
-  (rum/mount
-   [(app)]
+  (init-router)
+  (rd/render
+   (app)
    (js/document.getElementById "app")))
