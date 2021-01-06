8dd41c92c72e51ec0c6ce99bd019bb07b5fe0300
Malik Kennedy <mksybr@gmail.com>
2021-01-06 08:39:25 -0800

    refactor+bugfix+impl: make nav, fix fqn router, impl nav

--- src/clojure_scratchpad/frontend/router.cljs
+++ src/clojure_scratchpad/frontend/router.cljs
@@ -18,59 +18,26 @@
 
 (def routes
   [["/"
-    {:name ::frontpage
+    {:name :router/frontpage
      :view views/home-page}]
 
    ["/about"
-    {:name ::about
+    {:name :router/about
      :view views/about-page}]
 
    ["/portfolio"
-    {:name ::portfolio
+    {:name :router/portfolio
      :view views/portfolio-page}]
 
    ["/login"
-    {:name ::login
+    {:name :router/login
      :view views/login-form}]
 
-   
    ["/item/:id"
     {:name ::item
      :view views/item-page
      :parameters {:path {:id int?}
                   :query {(ds/opt :foo) keyword?}}}]])
-
-;; --
-(defn navigation []
-  (let [state (r/atom {:navbar-hidden true})]
-    (fn []
-      (let [is-hidden? (get @state :navbar-hidden)]
-        [:nav.navbar {:role "navigation"
-                      :aria-label "dropdown navigation"}
-         [:div.navbar-start
-          [:a.navbar-brand {:href (rfe/href ::frontpage)}
-           [:img {:src "logo.svg"}]]]
-         [:div.navbar-item.has-dropdown.is-hoverable
-          [:a.navbar-link
-           {:on-click
-            #(swap! state update :navbar-hidden not)}]
-          [:div.navbar-dropdown
-           (if is-hidden?
-             {:class "is-hidden"})
-           [:a.navbar-item
-            {:href (rfe/href ::frontpage)}
-            "home"]
-           [:a.navbar-item
-            {:href (rfe/href ::about)}
-            "blog"]
-           [:a.navbar-item
-            {:href (rfe/href ::portfolio)}
-            "portfolio"]
-           [:a.navbar-item
-            {:href "https://github.com/aeyk/clojure-scratchpad/"}
-            "source"]]]]))))
-;; --
-
 
 (defn init-router []
   (rfe/start!
--- src/clojure_scratchpad/frontend/views.cljs
+++ src/clojure_scratchpad/frontend/views.cljs
@@ -12,18 +12,85 @@
 (def MapContainer (r/adapt-react-class react-leaflet/MapContainer))
 (def TileLayer (r/adapt-react-class react-leaflet/TileLayer))
 
+(defn navigation []
+  (js/console.log )
+  (make-dropdown-navigation
+   "mksybr"
+   [["about"
+     (rfe/href :router/about)]
+    ["portfolio"
+     (rfe/href :router/portfolio)]
+    ["login"
+     (rfe/href :router/login)]])
+  #_(let [state (r/atom {:navbar-hidden true})]
+    (fn []
+      (let [is-hidden? (get @state :navbar-hidden)]
+        [:nav.navbar {:role "navigation"
+                      :aria-label "dropdown navigation"}
+         [:div.navbar-item.has-dropdown.is-hoverable
+          [:a.navbar-link
+           {:on-click #(swap! state update :navbar-hidden not)}
+           "A"]
+          [:div.navbar-dropdown (if is-hidden?
+                                  {:class "is-hidden"})
+           [:a.navbar-item "B"]
+           [:a.navbar-item "C"]]]]))))
+
+(defn make-dropdown-navigation [label dropdowns]
+  (let [state (r/atom {:navbar-hidden true})]
+    (fn []
+      (let [is-hidden? (get @state :navbar-hidden)]
+        [:nav.navbar {:role "navigation"
+                      :aria-label "dropdown navigation"}
+         [:div.navbar-item.has-dropdown.is-hoverable
+          [:a.navbar-link
+           {:on-click #(swap! state update :navbar-hidden not)}
+           label]
+          [:div.navbar-dropdown (if is-hidden?
+                                  {:class "is-hidden"})
+           (for [[label href] dropdowns]
+             [:a.navbar-item {:href href
+                              :key (gensym label)}
+              label])
+           #_#_[:a.navbar-item "B"]
+           [:a.navbar-item "C"]]]]))))
+
+(defn input
+  [label id type]
+  (let [state (atom "")]
+    [:div.field
+     [:div.control
+      [:label.label label]
+      [:input.input
+       {:type type
+        :id id
+        :value @state
+        :on-change (fn [e] (reset! state (.. e -target -value)))}]]]))
+
+
+(defn test-component
+  []
+  (let [submit-handler #(.preventDefault %)]
+    [:form
+     {:on-submit submit-handler}
+     [input"name:" "username" :text]
+     [input "password:" "password" :password]
+     [:input.button {:type :submit
+                     :on-submit submit-handler}]]))
+
 (defn home-page []
   [:div
+   [navigation]
    [:h1.title "Welcome"]
    [:p.subtitle (str  "Hello ")]
    [test-component]
    #_[MapContainer
-    {:center [27.77 -82.63] :zoom 13
-     :scrollWheelZoom false
-     :onClick (fn [e] (js/console.log e))}
-    [TileLayer
-     {:attribution "&copy <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors"
-      :url "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}]]])
+      {:center [27.77 -82.63] :zoom 13
+       :scrollWheelZoom false
+       :onClick (fn [e] (js/console.log e))}
+      [TileLayer
+       {:attribution "&copy <a href=\"http://osm.org/copyright\">OpenStreetMap</a> contributors"
+        :url "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"}]]])
 
 (defn input-field [label]
   (let [state (r/atom "")
@@ -63,23 +130,26 @@
           submit-handler}]]])))
 
 (defn portfolio-page []
-  [:div.card.is-half
-   [:div.card-content
-    [:p.subtitle "Twains uWu 🤪🤪"]
-    [:div.media-content
-     [:div.media-left
-      [:figure.image.is-48x48
-       [:img
-        {:src
-         "https://mksybr.com/portfolio/twains-demo.2.png"}]]]]]])
+  [:div
+   [navigation]
+   [:div.card.is-half
+    [:div.card-content
+     [:p.subtitle "Twains uWu 🤪🤪"]
+     [:div.media-content
+      [:div.media-left
+       [:figure.image.is-48x48
+        [:img
+         {:src
+          "https://mksybr.com/portfolio/twains-demo.2.png"}]]]]]]])
 
 (defn about-page []
   [:div
+   [navigation]
    #_[:input {:type :text
             :default-value (:name @state)
             :on-change (fn [e] (swap! name :name (.. e -target -value)))}]
    [:h2.title "About frontend"]
-   [input-field "name: "]
+   #_[input-field "name: "]
    #_[:ul
     [:li [:a {:href "http://google.com"} "external link"]]
     [:li [:a {:href (rfe/href ::foobar)} "Missing route"]]
@@ -98,35 +168,15 @@
      (if (:foo query)
        [:p "Optional foo query param: " (:foo query)])]))
 
-(defn input
-  [label id type]
-  (let [state (atom "")]
-    [:div.field
-     [:div.control
-      [:label.label label]
-      [:input.input
-       {:type type
-        :id id
-        :value @state
-        :on-change (fn [e] (reset! state (.. e -target -value)))}]]]))
-
 (defn login-form
   []
   (let [submit-handler #(.preventDefault %)]
-    [:form
-     {:on-submit submit-handler}
-     [input "name:" "username" :text]
-     [input "password:" "password" :password]
-     [:input.button {:type :submit
-                     :on-submit submit-handler}]]))
+    [:div
+     [navigation]
+     [:form
+      {:on-submit submit-handler}
+      [input "name:" "username" :text]
+      [input "password:" "password" :password]
+      [:input.button {:type :submit
+                      :on-submit submit-handler}]]]))
 
-
-(defn test-component
-  []
-  (let [submit-handler #(.preventDefault %)]
-    [:form
-     {:on-submit submit-handler}
-     [input "name:" "username" :text]
-     [input "password:" "password" :password]
-     [:input.button {:type :submit
-                     :on-submit submit-handler}]]))
