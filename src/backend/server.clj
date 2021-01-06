0ce29f1d52c9ad8592345cab4f9f0a32615e1dce
Malik Kennedy <mksybr@gmail.com>
2021-01-06 15:22:35 -0800

    chore: set up minimal ring reitit backend server

--- src/backend/server.clj
+++ src/backend/server.clj
@@ -2,36 +2,29 @@
   (:require [reitit.ring :as ring]
             [clojure.java.jdbc :as jdbc]
             [ring.adapter.jetty :as jetty]
-            [backend.layout :as layout]
             [ring.middleware.reload :refer [wrap-reload]]
             [ring.handler.dump :refer [handle-dump]]
             [ring.adapter.jetty :as jetty]
             [mount.core :as mount]))
 
-(defn home-page [{:keys [flash] :as request}]
-  (layout/render
-   request
-   "main.html"))
-   
-(def routes 
+(def routes
   [""
    {:middleware [;; todo middleware
                  ;; middleware/wrap-csrf
                  ;; middleware/wrap-formats
                  ]}                     
-   ["/" {:get home-page}]
+   ["/" {:get (constantly "Hello world!")}]
    #_["/ws" {:get about-page}]])
-
 
 (def app
   (ring/ring-handler
     (ring/router
-      routes)))
+     routes)))
 
-(defonce server (jetty/run-jetty (wrap-reload #'app) {:port 8080 :join? false}))
+(defonce server
+  (jetty/run-jetty
+   (wrap-reload #'app)
+   {:port 4000 :join? false}))
 
 (defn -main []
   (.start server))
-
-#_(.stop server)
-(-main)
