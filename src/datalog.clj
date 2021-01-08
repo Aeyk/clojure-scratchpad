8a1c652ea253fef6e1a22a41057413392525790e
Malik Kennedy <mksybr@gmail.com>
2021-01-08 14:33:01 -0800

    feat: todo list schema in datomic

--- src/datalog.clj
+++ src/datalog.clj
@@ -1,11 +1,6 @@
 (ns datalog
-  (:require [datalevin.core :as dl]
-            [datascript.core :as db]
-            [clojure.core.logic :refer :all]
-            [clojure.core.logic.fd :as fd]))
-
-(db/create-conn {})
-;; => #atom[#datascript/DB {:schema {}, :datoms []} 0x562a2fdd]
+  (:require
+   [datascript.core :as db]))
 
 
 (def db
@@ -18,3 +13,36 @@
      {:db/valueType :db.type/ref}})))
 
 (db/entity db 1)
+
+;; 
+(def schema-txn
+  [{:db/doc "Task unique ID"
+    :db/ident :task/id
+    :db/valueType :db.type/uuid
+    :db/unique :db.unique/identity
+    :db/cardinality :db.cardinality/one}
+   {:db/doc "Task name"
+    :db/ident :task/name
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+   {:db/doc "Task description"
+    :db/ident :task/description
+    :db/valueType :db.type/string
+    :db/cardinality :db.cardinality/one}
+   {:db/doc "Task state - not done/done"
+    :db/ident :task/state
+    :db/valueType :db.type/boolean
+    :db/cardinality :db.cardinality/one}
+   {:db/doc "Task last modified"
+    :db/ident :task/modified
+    :db/valueType :db.type/instant
+    :db/cardinality :db.cardinality/one}
+   {:db/doc "Task created"
+    :db/ident :task/created
+    :db/valueType :db.type/instant
+    :db/cardinality :db.cardinality/one}])
+
+(def db
+  (db/transact
+   (db/create-conn {}) 
+   schema-txn))
