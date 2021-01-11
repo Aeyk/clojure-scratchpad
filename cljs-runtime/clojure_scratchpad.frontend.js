43a11de3768d32a8445a723329e111b2eeec89dc
Malik Kennedy <mksybr@gmail.com>
2021-01-11 11:40:52 -0800

    chore: remove part of experiment where I copy around my bundled js

--- cljs-runtime/clojure_scratchpad.frontend.js
+++
@@ -1,9 +0,0 @@
-goog.provide('clojure_scratchpad.frontend');
-var module$node_modules$react_leaflet$cjs$index=shadow.js.require("module$node_modules$react_leaflet$cjs$index", {});
-clojure_scratchpad.frontend.init = (function clojure_scratchpad$frontend$init(){
-clojure_scratchpad.frontend.router.init_router();
-
-return reagent.dom.render.cljs$core$IFn$_invoke$arity$2(new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [clojure_scratchpad.frontend.router.current_page], null),document.getElementById("app"));
-});
-
-//# sourceMappingURL=clojure_scratchpad.frontend.js.map
